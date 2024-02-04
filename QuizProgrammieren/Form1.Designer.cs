eb5a50c999d415ced43c3f4277b881d15beeedb7
AdamitzP <158858880+AdamitzP@users.noreply.github.com>
2024-02-05 00:03:59 +0100

    Add files via upload
    Erster Commit

---
+++ QuizProgrammieren/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace QuizProgrammieren
+{
+    partial class Quiz
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblfrage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblfrage
+            // 
+            this.lblfrage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfrage.Location = new System.Drawing.Point(12, 390);
+            this.lblfrage.Name = "lblfrage";
+            this.lblfrage.Size = new System.Drawing.Size(776, 54);
+            this.lblfrage.TabIndex = 0;
+            this.lblfrage.Text = "Frage";
+            this.lblfrage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblfrage.Click += new System.EventHandler(this.lblfrage_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AntwortenPrüfen);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 475);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.AntwortenPrüfen);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 599);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(294, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.AntwortenPrüfen);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(431, 599);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(294, 66);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.AntwortenPrüfen);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(772, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Quiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 723);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblfrage);
+            this.Name = "Quiz";
+            this.Text = "Quiz";
+            this.Load += new System.EventHandler(this.Quiz_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblfrage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ QuizProgrammieren/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QuizProgrammieren
+{
+    public partial class Quiz : Form
+    {
+
+
+        // Fragen: Variablen
+
+
+        int richtigeAntwort;
+        int frageNummer = 1;
+        int score;
+        int prozent;
+        int anzahlFragen;
+        
+
+        public Quiz()
+        {
+            InitializeComponent();
+
+            FrageStellen(frageNummer);
+
+            anzahlFragen = 4;
+        }
+
+        private void lblfrage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AntwortenPrüfen(object sender, EventArgs e)
+        {
+            var senderObject  = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if(buttonTag == richtigeAntwort)
+            {
+                score++;
+            }
+
+            if(frageNummer == anzahlFragen)
+            {
+                //Prozentsatz
+
+                prozent = (int)Math.Round((double)(score * 100) / anzahlFragen);
+
+                MessageBox.Show(
+                    "Du hast das Ende des Quiz erreicht." + Environment.NewLine + 
+                    Environment.NewLine + "Du hast " + score + "/4 Fragen richtig beantwortet." + Environment.NewLine + 
+                    Environment.NewLine + "Damit hast du " + prozent + "% der Fragen richtig beantwortet." + Environment.NewLine +
+                    Environment.NewLine + "Drücke OK um das Quiz erneut zu starten!"
+                    );
+
+                score = 0;
+                frageNummer = 0;
+                FrageStellen(frageNummer);
+            }
+
+
+            frageNummer++;
+            FrageStellen(frageNummer);
+
+        }
+        private void FrageStellen(int FrageNummer)
+        {
+
+            switch(FrageNummer)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.images;
+
+                    lblfrage.Text = "1. Frage = A";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    richtigeAntwort = 1;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.images;
+
+                    lblfrage.Text = "2. Frage = B";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    richtigeAntwort = 2;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.images;
+
+                    lblfrage.Text = "3. Frage = C";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    richtigeAntwort = 3;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.images;
+
+                    lblfrage.Text = "4. Frage = D";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    richtigeAntwort = 4;
+
+                    break;
+            }
+
+
+
+        }
+
+        private void Quiz_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
